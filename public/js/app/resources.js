3afdb50037abeb585c4ae97dd02d7d55f6c261e4
theepicsnail <theepicsnail@gmail.com>
2014-07-01 18:39:22 -0400

    removing old

--- public/js/app/resources.js
+++
@@ -1,9 +0,0 @@
-define(["promise"], function(Promise) {
-  return {
-    load: function() {
-      return new Promise(function(a){a();});
-    }
-    // Provide whatever access to resources you want here.
-    // E.g. R.getModel('knight'), R.getIcon('close')... whatever
-  };
-});
--- public/js/loadscreen.js
+++
@@ -1,8 +0,0 @@
-define([], function() {
-  return {
-    start: function() {
-    },
-    stop: function() {
-    }
-  };
-});
