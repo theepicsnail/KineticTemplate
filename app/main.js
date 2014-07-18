e9d656770132ac7e8a7cee244a97c5091b2b491c
theepicsnail <theepicsnail@gmail.com>
2014-07-18 11:57:14 -0400

    setup a UI

--- app/main.js
+++ app/main.js
@@ -1,10 +1,28 @@
 require.config({
   baseUrl: "app",
   paths: {
+    jquery: "/bower_components/jquery/dist/jquery.min",
+    jquery_layout: "/bower_components/jquery.layout/dist/jquery.layout-latest.min",
+    jquery_ui: "/bower_components/jquery-ui/jquery-ui.min",
+  },
+  shim: {
+    jquery_layout: ['jquery', 'jquery_ui'],
+    jquery_ui: ['jquery']
   }
 });
 
-require(["state"],function(state) {
+require(["state", 'jquery', 'jquery_layout'],function(state, $){
   state.set("input", "hello world");
   document.state = state;
+
+  $('body').layout({applyDefaultStyles:true});
+  $('#input').on('keyup', function() {
+    state.set('input', $(this).val());
+  });
+  state.on('outputs', function(record) {
+    $("#output").text(record.val.stdout);
+  });
+  state.on('input', function(record) {
+    $("#input").val(record.val);
+  });
 });
