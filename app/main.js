1d241da0d065f34f609e53308b5577d1ee7502e3
theepicsnail <theepicsnail@gmail.com>
2014-07-18 20:13:42 -0400

    Now using ace editor in place of textarea

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 require.config({
   baseUrl: "app",
   paths: {
+    ace: "/bower_components/ace/lib/ace",
     jquery: "/bower_components/jquery/dist/jquery.min",
     jquery_layout: "/bower_components/jquery.layout/dist/jquery.layout-latest.min",
     jquery_ui: "/bower_components/jquery-ui/jquery-ui.min",
@@ -11,18 +12,40 @@ require.config({
   }
 });
 
-require(["state", 'jquery', 'jquery_layout'],function(state, $){
-  state.set("input", "hello world");
+require(["ace/ace", "state", 'jquery', 'jquery_layout'],function(ace, state, $){
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/plain_text");
   document.state = state;
 
   $('body').layout({applyDefaultStyles:true});
-  $('#input').on('keyup', function() {
-    state.set('input', $(this).val());
+
+  var release_lock = 0;
+  $('#input').on('keyup', function(event) {
+    if(!state.held('input')) {
+      state.grab('input').then(function(){
+        state.set('input', $(this).val());
+        if(release_lock)
+          clearTimeout(release_lock);
+
+        release_lock = setTimeout(function(){
+          console.log("release");
+          state.release('input');
+        }, 1000);
+
+      });
+    } else {
+//      state.set('input', $(this).val());
+      event.preventDefault();
+    }
   });
+
   state.on('outputs', function(record) {
     $("#output").text(record.val.stdout);
   });
+
   state.on('input', function(record) {
-    $("#input").val(record.val);
+    console.log("input update", record);
+    //$("#input").val(record.val);
   });
 });
